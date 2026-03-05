8918665914847c08da3efee63949577eee3e3cd3
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2026-03-05 17:47:11 +0800

    fix(init_user.sh): typo `copy_ssh_folder.sh` to `copy_ssh_files.sh`

--- init_user.sh
+++ init_user.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 $SCRIPT_DIR/sshd_add_pub_key.sh
 $SCRIPT_DIR/setup_podman.sh
-$SCRIPT_DIR/copy_ssh_folder.sh
+$SCRIPT_DIR/copy_ssh_files.sh
 $SCRIPT_DIR/setup_ssh-agent.sh
